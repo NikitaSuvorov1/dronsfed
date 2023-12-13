0afd4631ef29478756f15a11d0cb58b7f71a49d2
NikitaSuvorov1 <86976882+NikitaSuvorov1@users.noreply.github.com>
2023-12-13 20:22:08 +0300

    fix1312

--- src/Components/General/index.jsx
+++ src/Components/General/index.jsx
@@ -7,7 +7,7 @@ export const General = () => {
             <div className={styles.leftcontent}>
                 <div className={styles.ellipse1}  />
                 <p className={styles.maintitle}>ФЕДЕРАЦИЯ ГОНОК ДРОНОВ РЕСПУБЛИКИ ТАТАРСТАН</p>
-                <p className={styles.subtitle}>kazan 2024</p>
+                <p className={styles.subtitle}>Kazan 2024</p>
                 <p className={styles.generalText}>Взлетай выше, летай быстрее, вместе с Федерацией гонок дронов Республики
                     Татарстан!</p>
                 <div loading={"lazy"} className={styles.ellipse2}  />
